11fac97f675b3bea63600457b9d802f5637e9f19
Ram Basnet <rambasnet@gmail.com>
2021-04-08 16:10:17 -0600

    fixed compilation issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Banner from './components/banner';
+import Resarch from './components/research';
+import Home from './components/home';
+import Navigation from './components/navigation';
+import Footer from './components/footer';
 
 function App() {
   return (
     <>
+    
     </>
   );
 }
