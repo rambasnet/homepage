77209fed3dd162361266ec068b09ad8195ffbff1
Ram Basnet <rambasnet@gmail.com>
2021-04-08 15:37:15 -0600

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
 
-
-import Banner from './components/banner';
-import Footer from './components/footer';
-import Navbar from './components/navigation';
-
-import Teaching from './components/schedule';
-import Resources from './components/resources';
-
 function App() {
   return (
     <>
